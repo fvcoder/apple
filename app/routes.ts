d23c8eeae0e1c715f0a3adaa9676ac8d8f6d3dff
Fernando Ticona <thefersh24@gmail.com>
2026-02-22 19:33:19 -0400

    fix: corregir la definición de la ruta principal en routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
-import { layout, route, type RouteConfig } from "@react-router/dev/routes";
+import { index, layout, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  route("apple", "routes/home.tsx"),
+  index("routes/home.tsx"),
   layout("./core/layout/explorer.layout.tsx", [
     route("/system-experiences/live-activities", "./system-experiences/view/live-activities.tsx"),
   ]),
