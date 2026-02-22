210d52440767cc357411ecea75a00380ab8721b7
Fernando Ticona <thefersh24@gmail.com>
2026-02-22 13:03:06 -0400

    chore: update dependencies in pnpm-lock.yaml and modify basename in react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,5 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: false,
-  basename: import.meta.env.PROD ? "https://fvcoder.github.io/apple/" : "/",
+  basename: process.env.PUBLIC_URL,
 } satisfies Config;
