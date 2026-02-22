946fd96caacb1183713603190874c10255ffb2f2
Fernando Ticona <thefersh24@gmail.com>
2026-02-22 12:36:21 -0400

    feat: agregar icono de lupa para la funcionalidad de búsqueda en LauncherApp; ajustar estilos y estructura del componente

--- app/device/apps/launcher/index.tsx
+++ app/device/apps/launcher/index.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 
+import { SfMagnifyingGlass } from "~/core/icons/sfMagnifyingglass";
 import { useDevice } from "~/core/state/device.state";
 import { iPhoneSafeAreaInsets } from "~/device/iphone/data/constants";
 import type { AppOptions } from "~/device/types";
@@ -16,17 +17,24 @@ const LauncherApp: () => React.ReactNode = () => {
       style={{ backgroundImage: `url(public/iphone/bg/iClarified-iOS26-Sky-HomeScreen-Light.jpg)` }}
     >
       <div
-        className="flex size-full flex-col"
+        className="flex size-full flex-col gap-5"
         style={{
           padding: `${safeAreaInsets.top}px ${safeAreaInsets.right}px ${safeAreaInsets.bottom}px ${safeAreaInsets.left}px`,
         }}
       >
-        <main className="flex-1">
-          <h1 className="text-2xl font-bold text-white">Launcher</h1>
-          {JSON.stringify(safeAreaInsets)}
+        <main className="flex flex-1 flex-col gap-5">
+          <div className="flex-1">
+            <div className="size-full rounded-2xl bg-red-500">asa</div>
+          </div>
+          <button className="mx-auto w-fit">
+            <div className="flex size-full h-8 items-center justify-center gap-1.5 rounded-2xl border border-white/20 px-3 text-white backdrop-blur-xs">
+              <SfMagnifyingGlass width={12} />
+              <span className="font-sf-ui text-xs select-none">Buscar</span>
+            </div>
+          </button>
         </main>
 
-        <footer className="mx-auto mb-4.5 grid h-26.25 w-94.5 grid-cols-4 gap-6 overflow-hidden rounded-[40px] border border-white/50 px-4.5 py-5 backdrop-blur-xs">
+        <footer className="mx-auto mb-4.5 grid h-26.25 w-94.5 grid-cols-4 gap-6 overflow-hidden rounded-[40px] border border-white/20 px-4.5 py-5 backdrop-blur-xs">
           <div className="size-full rounded-2xl bg-red-500">asa</div>
           <div className="size-full rounded-2xl bg-red-500">asa</div>
           <div className="size-full rounded-2xl bg-red-500">asa</div>
