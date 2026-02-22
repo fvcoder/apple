8d67a5c6ef9de18f3a7897584107fe1250fefe74
Fernando Ticona <thefersh24@gmail.com>
2026-02-22 12:42:41 -0400

    feat: agregar base href dinámico en Layout y corregir la ruta de índice en routes; actualizar homepage en package.json

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
-import { index, layout, route, type RouteConfig } from "@react-router/dev/routes";
+import { layout, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  route("apple", "routes/home.tsx"),
   layout("./core/layout/explorer.layout.tsx", [
     route("/system-experiences/live-activities", "./system-experiences/view/live-activities.tsx"),
   ]),
